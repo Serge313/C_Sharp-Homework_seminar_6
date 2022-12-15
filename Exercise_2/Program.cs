7f4ce1a4a6aba5de9f8b96775ed431693228fef5
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-12-15 19:52:42 +0300

    Test added to Exercise_2

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -1,7 +1,49 @@
-﻿double b1 = GetNumber("Enter value of \"b1\" ");
+﻿TestingFindXAndFindY();
+double b1 = GetNumber("Enter value of \"b1\" ");
 double k1 = GetNumber("Enter value of \"k1\" ");
 double b2 = GetNumber("Enter value of \"b2\" ");
 double k2 = GetNumber("Enter value of \"k2\" ");
+
+
+void TestingFindXAndFindY()
+{
+    Console.WriteLine("Testing of \"FindX\" and \"FindY\" methods has been launched... ");
+    double testB1 = 2;
+    double testK1 = 5;
+    double testB2 = 4;
+    double testK2 = 9;
+    double expectedX = -0.5;
+    double actualX = FindX(testB1, testK1, testB2, testK2);
+    double expectedY = -0.5;
+    double actualY = FindY(testK1, testB1, actualX);
+    bool isEqual = expectedX == actualX && expectedY == actualY;
+    if (isEqual)
+    {
+        Console.WriteLine("Test completed successfully!");
+    }
+    else
+    {
+        Console.WriteLine("Error! Need to fix the method!");
+    }
+    Console.WriteLine();
+    
+}
+
+
+double FindX(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return (x);
+}
+
+
+double FindY(double k1, double b1, double x)
+{
+    double y = k1 * x + b1;
+    return(y);
+}
+
+
 IntersectOrNot(b1, k1, b2, k2);
 
 
@@ -19,19 +61,7 @@ void IntersectOrNot(double b1, double k1, double b2, double k2)
     }
     else
     {
-        double FindX(double b1, double k1, double b2, double k2)
-        {
-            double x = (b2 - b1) / (k1 - k2);
-            return (x);
-        }
         double x = FindX(b1, k1, b2, k2);
-
-        
-        double FindY(double k1, double b1, double x)
-        {
-            double y = k1 * x + b1;
-            return(y);
-        }
         double y = FindY(k1, b1, x);
         Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x};{y})");
     }
